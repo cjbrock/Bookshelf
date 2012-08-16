7eec69aaa98d74287ec113951039a1da0a90ac86
Corinna Brock <corinna.brock@gmail.com>
2012-08-16 13:02:10 -0400

    wrote the standard models

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,64 @@
 class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @author }
+    end
+  end 
+
+  def show
+    @author = Author.find(params[:id])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @author }
+  end
+
+  def new
+    @author = Author.new
+    respond_to do |format|
+      format.html
+      format.json { render json: @author }
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def create
+    @author = Author.new(params[:author])
+      respond_to do |format|
+      if @author.save
+        format.html { redirect_to @author, notice: "Another creative genius" }
+        format.json { render json: @author, status: :created, location: @author }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    respond_to do |format|
+      if @author.update_attributes(params[:author])
+        format.html { redirect_to @author, notice: "Author updated!" }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+    respond_to do |format|
+      format.html { redirect_to authors_url }
+      format.json { head :no_content }
+    end
+  end
+end
 end
