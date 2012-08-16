a4b45999bfc6a2f5fed4ea0aac3c2d25517f5e6a
Corinna Brock <corinna.brock@gmail.com>
2012-08-16 12:30:23 -0400

    created Author resource.  playing with bootstrap formatting.

---
+++ app/models/author.rb
@@ -0,0 +1,3 @@
+class Author < ActiveRecord::Base
+  attr_accessible :bio, :first_name, :last_name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bookshelf::Application.routes.draw do
+  resources :authors
+
   resources :books
 
   # The priority is based upon order of creation:
