b84d010df4fb2a84a15df3e2df90e2f1e81fa84b
Corinna Brock <corinna.brock@gmail.com>
2012-08-22 16:10:43 -0400

    fixed the users routing problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to root_path, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
